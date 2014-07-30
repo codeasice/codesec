698b4ba8de39e5ae670b3c4ffb8d43f550d58ff4
cody <github@codylambert.com>
2014-07-29 22:41:23 -0400

    Added setting menu item and replaced hardcoded fayeURL w settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140728012734) do
+ActiveRecord::Schema.define(:version => 20140729030016) do
+
+  create_table "settings", :force => true do |t|
+    t.string   "fayeURL"
+    t.string   "floorplan"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "zone_statuses", :force => true do |t|
     t.integer  "zone"
