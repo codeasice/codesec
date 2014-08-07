e8a0b1630ed6d79095baa1c5380c915561834fb5
Cody Lambert <github@codylambert.com>
2014-08-06 20:44:17 -0400

    Cleaned up diagnostics and dashboard routes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  def home
+  def index
   end
  
   def sparklines
--- config/routes.rb
+++ config/routes.rb
@@ -11,68 +11,14 @@ Codesec::Application.routes.draw do
 
   get "dashboard/testing"
 
-  get "dashboard/home"
+  get "dashboard/index"
 
   resources :zone_statuses
 
  match '/recent', to: 'zone_statuses#recent', via: :get
  match '/sparklines', to: 'dashboard#sparklines', via: :get
+ match '/dashboard', to: 'dashboard#index', via: :get
+ match '/diagnostics', to: 'diagnostics#home', via: :get
+root :to => 'dashboard#index'
 
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-root :to => 'dashboard#home'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
